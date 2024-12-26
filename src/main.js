a94c26ed243535f631ee9aea5e261e782eaa1505
wutiaowu5t5 <1987053649@qq.com>
2024-12-26 18:49:50 +0800

    refactor: change Main JS file, app Vue file

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
+// 初始化导入vue
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+const VueController = createApp(App)
 
-createApp(App).mount('#app')
+// 绑定dom节点实例
+VueController.mount('#app')
