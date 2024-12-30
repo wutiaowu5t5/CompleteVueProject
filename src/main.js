81acdb3e3e60a23b680270e7724567f1053ce463
wutiaowu5t5 <1987053649@qq.com>
2024-12-30 17:03:58 +0800

    chore: the new vue-router library distinguishes between dynamic routing and static routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 // 初始化导入vue
+// example initialize the imported vue
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 const VueController = createApp(App)
 
+// 挂载vue-router
+// mount the vue router
+import router from '@router/router.js'
+VueController.use(router)
+
 // 绑定dom节点实例
+// bind dom node instances
 VueController.mount('#app')
